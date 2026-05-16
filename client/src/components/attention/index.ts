612b852c19b898456c0f939562ba2943819af7be
thompson <hi.thompson@hotmail.com>
2026-05-16 12:02:09 +0100

    chore: export filterDismissedNotifications from attention barrel

--- client/src/components/attention/index.ts
+++ client/src/components/attention/index.ts
@@ -3,4 +3,5 @@ export { FilterBadges } from "./FilterBadges";
 export { NotificationPanel } from "./NotificationPanel";
 export { NotificationItem } from "./NotificationItem";
 export { deriveNotificationEntries } from "./derive-notifications";
+export { filterDismissedNotifications } from "./filter-dismissed-notifications";
 export type { NotificationEntry } from "./types";
